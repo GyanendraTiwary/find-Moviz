a36b36d3a3a4a791a4b1c6176464765af2af9159
Gyanendra <gyanendrat1234@gmail.com>
2023-11-25 19:35:36 +0530

    fixed search btn

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -28,6 +28,14 @@ const HeroBanner = () => {
         }
     };
 
+    function searchBtnHandler() {
+        if (query.length > 0) {
+            navigate(`/search/${query}`);
+        }
+    };
+
+    
+
     return (
         <div className="heroBanner">
             {!loading && (
@@ -51,7 +59,7 @@ const HeroBanner = () => {
                             onChange={(e) => setQuery(e.target.value)}
                             onKeyUp={searchQueryHandler}
                         />
-                        <button>Search</button>
+                        <button onClick={searchBtnHandler}>Search</button>
                     </div>
                 </div>
             </ContentWrapper>
